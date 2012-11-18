9797b4c07cbfd3e8710c6a22af11a6a17d48485c
Sophie Lange <sophie_jeanne_la_douce@yahoo.com>
2012-11-18 04:15:13 -0800

    I did the stuff before like a month ago. So now I know more about functions and for.
    The old stuff was kind of hard to understand,, especially the main loop part. I just got it to work, but now I'm like wtf is that.
    
    So I was doing this again, and so it's not so long and confusing.

--- lights.ino
+++ lights.ino
@@ -1,8 +1,6 @@
-int show = 1;
-int times = 1;
-
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() 
+{
   // initialize the digital pin as an output.
   pinMode( 5, OUTPUT);     
   pinMode( 6, OUTPUT);     
@@ -14,154 +12,113 @@ void setup() {
   pinMode(12, OUTPUT);     
 }
 
-void show1() {
-  digitalWrite( 5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite( 9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 5, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 9, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 6, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 6, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 7, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 8, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 9, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(6, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(6, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+void all_lights_off()
+{
+  digitalWrite(5, LOW);     
+  digitalWrite(6, LOW);     
+  digitalWrite(7, LOW);     
+  digitalWrite(8, LOW);     
+  digitalWrite(9, LOW);     
+  digitalWrite(10, LOW);     
+  digitalWrite(11, LOW);     
+  digitalWrite(12, LOW);     
 }
 
-void show2() {
-  digitalWrite( 5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite( 6, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 5, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 6, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 6, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 6, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 8, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 9, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+void pair_on(int first, int second)
+{
+  digitalWrite(first, HIGH);     
+  digitalWrite(second, HIGH);     
 }
 
+void pair_off(int first, int second)
+{
+  digitalWrite(first, LOW);     
+  digitalWrite(second, LOW);     
+}
+
+// Show #1 is opposite lights going around.
+void show1() 
+{
+  all_lights_off();
+  pair_on(5, 9);
+  delay(100);               
+  pair_off(5, 9);
+  pair_on(6, 10);
+  delay(100);               
+  pair_off(6, 10);
+  pair_on(7, 11);
+  delay(100);               
+  pair_off(7, 11);
+  pair_on(8, 12);
+  delay(100);               
+  pair_off(8, 12);
+  pair_on(9, 5);
+  delay(100);               
+  pair_off(9, 5);
+  pair_on(10, 6);
+  delay(100);               
+  pair_off(10, 6);
+  pair_on(11, 7);
+  delay(100);               
+  pair_off(11, 7);
+  pair_on(12, 8);
+  delay(100);               
+}
+
+// Show #2 is two lights next to each other going around. 
+void show2() 
+{
+  all_lights_off();
+  pair_on(5,6 );
+  delay(100);               
+  pair_off(5,6 );
+  pair_on(6,7 );
+  delay(100);               
+  pair_off(6,7 );
+  pair_on(7,8 );
+  delay(100);               
+  pair_off(7,8 );
+  pair_on(8,9 );
+  delay(100);               
+  pair_off(8,9 );
+  pair_on(9,10 );
+  delay(100);               
+  pair_off(9,10 );
+  pair_on(10, 11 );
+  delay(100);               
+  pair_off(10, 11 );
+  pair_on(11,12 );
+  delay(100);               
+  pair_on(12,5 );
+  delay(100);               
+}
+
+// Show #3 is every other light on then off
 void show3()
 {
-  digitalWrite( 5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 5, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 7, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite( 6, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite( 6, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(12, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
+  all_lights_off();
+  digitalWrite( 5, HIGH);   
+  digitalWrite(7, HIGH);   
+  digitalWrite(9, HIGH);   
+  digitalWrite(11, HIGH);   
+  delay(100);               
+  all_lights_off();
+  digitalWrite( 6, HIGH);   
+  digitalWrite(8, HIGH);   
+  digitalWrite(10, HIGH);   
+  digitalWrite(12, HIGH);   
+  delay(100);               
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
-  if(show==1) {
-  if(times==5){
-  show=2;
-  times=1;
-  }
-  else
-  if(times<5){
-  times=times+1;
-  }
-  }
-  if(show==2) {
-  if(times==5){
-  show=3;
-  times=1;
-  }
-  else
-  if(times<5){
-  times=times+1;
-  }
-  }
-  if(show==3) {
-  if(times==10){
-  show=1;
-  times=1;
-  }
-  else
-  if(times<10){
-  times=times+1;
-  }
-  }
-  if(show == 1) {
+void loop() 
+{
+  int i;
+  for(i=0; i<6; i++) 
     show1();
-  }
-  if(show == 2) {
+  for(i=0; i<6; i++) 
     show2();
-  }
-  if(show == 3) {
+  for(i=0; i<15; i++) 
     show3();
-  }
 }
+
